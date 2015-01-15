6fba123f3ce5c806017acaef63aee398c05e9c8b
Premysl Donat <donat@uol.cz>
2015-01-15 08:30:54 +0100

    Upgrade Grape version

--- lib/grape/validations/validations.rb
+++ lib/grape/validations/validations.rb
@@ -1,5 +1,5 @@
 module Grape::Validations
-  class PaginationPageValidator < Validator
+  class PaginationPageValidator < Base
     def validate_param!(attr_name, params)
       unless params[attr_name].to_i >= 1
         throw :error, :status => 400, :message => "The page param must have a value of 1 or greater."
